ceae2ccd690ed3eb397f1a3dd8a1672bed56a5cf
Тимофей Клоков <tima-klokov@mail.ru>
2021-08-17 14:01:35 +0300

    Authentication added

---
+++ app/src/main/java/com/siriusproject/coshelek/OnBoardingActivity.kt
@@ -0,0 +1,55 @@
+package com.siriusproject.coshelek
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+
+class OnBoardingActivity : AppCompatActivity() {
+
+    private val loginResultHandler =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            val task = GoogleSignIn.getSignedInAccountFromIntent(result?.data)
+            val account = task.result
+
+            if (account != null) {
+                startWalletActivity()
+            } else {
+                Toast.makeText(this, resources.getString(R.string.auth_failed), Toast.LENGTH_LONG)
+                    .show()
+            }
+        }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.onboarding_screen)
+
+        val googleButton = findViewById<Button>(R.id.authButton)
+
+        googleButton.setOnClickListener {
+            loginResultHandler.launch(getSignInIntent())
+        }
+    }
+
+    private fun getSignInIntent(): Intent {
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+
+        val mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        return mGoogleSignInClient.signInIntent
+    }
+
+    private fun startWalletActivity() {
+        val intent = Intent(this, MainScreenActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
+        startActivity(intent)
+        finish()
+    }
+}
--- app/src/main/java/com/siriusproject/coshelek/SplashScreenActivity.kt
+++ app/src/main/java/com/siriusproject/coshelek/SplashScreenActivity.kt
@@ -3,11 +3,29 @@ package com.siriusproject.coshelek
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        startActivity(Intent(this, MainActivity::class.java))
+
+        val account = GoogleSignIn.getLastSignedInAccount(this)
+        if (account != null) {
+            startMainScreen()
+        } else {
+            startOnBoard()
+        }
         finish()
+    }
+
+    private fun startOnBoard() {
+        val intent = Intent(this, OnBoardingActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun startMainScreen() {
+        val intent = Intent(this, MainScreenActivity::class.java)
+        startActivity(intent)
     }
 }
