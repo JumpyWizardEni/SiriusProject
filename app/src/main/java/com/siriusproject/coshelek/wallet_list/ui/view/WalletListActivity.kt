fac8800381a20095afbee55f2692609561fcd772
Тимофей Клоков <tima-klokov@mail.ru>
2021-08-20 21:40:36 +0300

    resolves #29 Main screen for wallets list added

---
+++ app/src/main/java/com/siriusproject/coshelek/wallet_list/ui/view/WalletListActivity.kt
@@ -0,0 +1,7 @@
+package com.siriusproject.coshelek.wallet_list.ui.view
+
+import androidx.appcompat.app.AppCompatActivity
+import com.siriusproject.coshelek.R
+
+class WalletListActivity : AppCompatActivity(R.layout.activity_wallet_list) {
+}
